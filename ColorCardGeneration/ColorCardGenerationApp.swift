433b7915d74c8f48c36d9503f2456d14f01ed14d
Shreya Prasad <prasadshreya2303@gmail.com>
2025-07-31 08:31:35 +0530

    Initial Commit

---
+++ ColorCardGeneration/ColorCardGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCardGenerationApp.swift
+//  ColorCardGeneration
+//
+//  Created by Shreya Prasad on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorCardGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorCardGeneration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorCardGeneration
+//
+//  Created by Shreya Prasad on 31/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
