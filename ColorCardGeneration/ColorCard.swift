395bf4b0455318d3315d9027c9d9a72e2e7a9040
Shreya Prasad <prasadshreya2303@gmail.com>
2025-07-31 08:38:43 +0530

    first commit

---
+++ ColorCardGeneration/ColorCard.swift
@@ -0,0 +1,18 @@
+//
+//  ColorCard.swift
+//  ColorCardGeneration
+//
+//  Created by Shreya Prasad on 31/07/25.
+//
+
+import SwiftUI
+
+struct ColorCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorCard()
+}
---
+++ ColorCardGeneration/ColorRecord.swift
@@ -0,0 +1,8 @@
+//
+//  ColorRecord.swift
+//  ColorCardGeneration
+//
+//  Created by Shreya Prasad on 31/07/25.
+//
+
+import Foundation
